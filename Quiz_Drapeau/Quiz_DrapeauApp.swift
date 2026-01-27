0df7cfe69fc7b236ddd41adc943a8ff4675b1f67
Kryss <rayanenana075@gmail.com>
2026-01-27 12:43:16 -0500

    Initial Commit

---
+++ Quiz_Drapeau/Quiz_DrapeauApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_DrapeauApp.swift
+//  Quiz_Drapeau
+//
+//  Created by user289221 on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_DrapeauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
