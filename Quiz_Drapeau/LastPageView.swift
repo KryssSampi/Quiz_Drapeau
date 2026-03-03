e0b506bd1f93253c536d5c38b63c997431ed771f
KryssSampi <SampiKryss@gmail.com>
2026-03-03 03:14:26 -0500

    Add assets and initial SwiftUI views for Canadian flag quiz app
    - Added images for provinces: Alberta, Colombie-Britannique, Ile-du-Prince-Edouard, Manitoba, Nouveau-Brunswick, Nouvelle-Ecosse, Nunavut, Ontario, Quebec, Saskatchewan, Terre-Neuve, Territoire du Nord-Ouest, Yukon, and a medal.
    - Created Contents.json files for each image set to define image properties.
    - Implemented initial SwiftUI views: LastPageView, MainView, and QuestionPageTemplate.
    - Defined a Question struct and an array of questions for the quiz functionality.

---
+++ Quiz_Drapeau/LastPageView.swift
@@ -0,0 +1,18 @@
+//
+//  LastPageView.swift
+//  Quiz_Drapeau
+//
+//  Created by user289221 on 1/27/26.
+//
+
+import SwiftUI
+
+struct LastPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LastPageView()
+}
